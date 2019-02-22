37f08e67775d00a60a91afb0bd95f1e8ee79f43a
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-02-22 10:47:17 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+
+
+var flipCard = function(cardId) {
+checkForMatch();
+
+if (cardsInPlay.length === 2) {
+
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+
+} else {
+	alert("Sorry, try again.");
+}
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+}
+
+flipCard(0);
+flipCard(2);
+
